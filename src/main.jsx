9df9a57dfc3af85e11b54cb7c1c2cd26e1ad0d76
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-05-01 11:31:49 +0300

    css done5

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './index.css'
 
+
+
 ReactDOM.createRoot(document.getElementById('root'), {
   identifierPrefix: 'contacts-book-app-'
 }).render(
